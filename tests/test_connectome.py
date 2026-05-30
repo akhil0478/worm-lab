51f1446cea1e7115346ece5e1e8932329f5879a7
“Krishnagit config --global user.email “darklight1035@gmailcom <darklight1035@gmail.com>
2026-05-30 10:29:10 +0530

    update connectome test to use real dataset

--- tests/test_connectome.py
+++ tests/test_connectome.py
@@ -3,7 +3,7 @@ from src.connectome.loader import load_connectome
 
 def test_load_connectome():
     G = load_connectome(
-        "data/raw/connectome_dataset/connectome.csv"
+        "data/raw/connectome_dataset/NeuronConnect1.xls"
     )
 
     assert G.number_of_nodes() > 0
