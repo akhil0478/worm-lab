12f87e85622d87380947f5410c83737219af577d
“Krishnagit config --global user.email “darklight1035@gmailcom <darklight1035@gmail.com>
2026-08-18 14:20:38 +0530

    food world updated

--- src/environment/food_world.py
+++ src/environment/food_world.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
-from typing import Callable
+
+import numpy as np
 
 
 @dataclass
 class FoodWorld:
-    food_function: Callable[[float, float], float]
+
+    def food_function(self, x: float, y: float) -> float:
+        return np.exp(-(x**2 + y**2) / 2)
 
     def query(self, x: float, y: float) -> float:
         return self.food_function(x, y)
